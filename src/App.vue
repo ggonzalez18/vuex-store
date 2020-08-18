e26b92afaba1a048d39369a44deb72e6ce49d6e5
Gabriela Gonzalez <gabytasmile7@gmail.com>
2020-08-17 22:56:06 -0400

    creamos un input para filtrar productos

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <p v-text="busy"></p>
     <p v-text="totalSale"></p>
-    <ul>
+
+    <input type="text" placeholder="Buscar" v-model="search">
+    <ul v-if="searchById">
+      <li>{{searchById.name}}</li>
+      </ul>
+    <ul v-else>
       <li v-for="(product, index) in availableProducts" :key="index">{{product.name}}</li>
     </ul>
     <p><b>Ofertas de ultimo Minuto</b></p>
@@ -18,10 +23,15 @@
 import {mapState, mapGetters} from 'vuex'
 export default {
   name: 'App',
+  data() {
+    return {
+      search: null
+    }
+  },
   computed: {
     ...mapState(["isBusy", "sales"]),
      // permite acceder a los metodos sin tanta busqueda (this.bla bla) referenciando al store y variable state
-     ...mapGetters(['availableProducts', 'cheapProducts']),
+     ...mapGetters(['availableProducts', 'cheapProducts', 'getProductById']),
     busy() {
       let statusBusy = this.isBusy ? 'ocupado' : 'disponible'
       return `Estado ${statusBusy}`
@@ -29,6 +39,9 @@ export default {
     totalSale() {
       return `El total de ventas es: ${this.sales}`
     },
+    searchById() {
+      return this.getProductById(this.search)
+    }
   }
 }
 
